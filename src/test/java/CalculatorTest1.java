19008bc51d97ed97b7a97f1538d8306b3f42e054
Nataliia Zaitseva <Nataliia.Zaitseva@onseo.biz>
2021-03-16 20:37:48 +0200

    add files#

---
+++ src/test/java/CalculatorTest1.java
@@ -0,0 +1,38 @@
+import junit.framework.TestCase;
+import org.junit.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class CalculatorTest1  extends TestCase{
+     private Calculator calculator = new Calculator();
+
+
+
+//    @Before
+//    public  void initTest() {
+//
+//    }
+//
+//    @After
+//    public void afterTest() {
+//        calculator = null;
+//    }
+
+    @Test
+    public void testGetSum()  throws Exception {
+        assertEquals(15, calculator.getSum(5, 10));
+
+    }
+
+    @Test
+    public void testGetDivide() throws Exception {
+        assertEquals(6, calculator.getDivide(12, 2));
+    }
+
+    @Test
+    public void testGetMultiple() throws Exception {
+        assertEquals(9, calculator.getMultiple(3, 3));
+    }
+}
